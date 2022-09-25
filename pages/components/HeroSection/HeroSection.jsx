3871835ea513543ee02b3d3b524cd8b37cf872c2
Yin Lynn Htun <yinlynnhtun@better.hr>
2022-09-25 19:36:57 +0630

    feat: Added fade in animation for items

--- pages/components/HeroSection/HeroSection.jsx
+++ pages/components/HeroSection/HeroSection.jsx
@@ -1,10 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
-import { motion, useScroll } from 'framer-motion'
+import { motion, useScroll, useInView } from 'framer-motion'
 import OfferSection from './OfferSection'
+import ValueItem from './ValueItem'
 
 const HeroSection = () => {
     const ref = useRef(null)
+    const isInView = useInView(ref)
+
     const { scrollYProgress } = useScroll({
         target: ref,
         offset: ['start end', 'end end'],
@@ -12,13 +16,30 @@ const HeroSection = () => {
 
     return (
         <div className="relative pb-20">
-            <div className="h-[100vh] sticky top-0 left-0 w-full -z-10 opacity-90">
+            <div className="h-[calc(100vh-47px)] sticky top-[47px] left-0 w-full -z-10 opacity-90">
                 <Image
                     className="object-cover"
                     layout="fill"
                     alt="image"
-                    src="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80"
+                    src="https://www.apple.com/v/apple-tv-plus/ac/images/overview/hero_startframe__fs1yactuf3qm_small.jpg"
                 />
+            </div>
+            <div className="absolute top-[57vh] left-0 px-6">
+                <h2 className="text-[40px] leading-[44px] font-extrabold  text-white">
+                    <p>
+                        All Apple Originals. {/* <br class="large" /> */}
+                        Only on&nbsp;Apple&nbsp;TV+.
+                    </p>
+                </h2>
+                <div className="mt-16 text-gray-300 font-semibold">
+                    Watch on the{' '}
+                    <img
+                        className="w-10 h-10 inline-block"
+                        src="https://www.apple.com/v/apple-tv-plus/ac/images/overview/hero_icon__gpaz9xyhw0uq_large_2x.png"
+                        alt="Logo"
+                    />{' '}
+                    app
+                </div>
             </div>
             <motion.div
                 ref={ref}
@@ -30,23 +51,13 @@ const HeroSection = () => {
             ></motion.div>
             <motion.div>
                 <section className="px-6 relative">
-                    <h2 className="text-4xl font-extrabold  text-white">
-                        <p>
-                            All Apple Originals.
-                            <br />
-                            Only on&nbsp;Apple&nbsp;TV+.
-                        </p>
-                    </h2>
-                    <div className="mt-16 text-gray-300 font-semibold">
-                        Watch on the Tv app
-                    </div>
                     <ul className="font-bold text-white text-[28px] flex flex-col gap-12">
-                        <li>New Apple&nbsp;Originals every&nbsp;month.</li>
-                        <li>
-                            Stream on the Apple&nbsp;TV&nbsp;app on Apple
-                            devices, smart&nbsp;TVs, consoles, or&nbsp;sticks.
-                        </li>
-                        <li>Share Apple&nbsp;TV+ with your&nbsp;family.</li>
+                        <ValueItem text="New Apple Originals every month." />
+                        <ValueItem
+                            text="Stream on the Apple TV app on Apple
+                            devices, smart TVs, consoles, or sticks."
+                        />
+                        <ValueItem text="Share Apple TV+ with your family." />
                     </ul>
                 </section>
                 <OfferSection />
--- pages/components/HeroSection/OfferItems.jsx
+++ pages/components/HeroSection/OfferItems.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useInView } from 'framer-motion'
+import React, { useRef } from 'react'
 
 const OfferItems = ({ heading, title, subtitle, buttonText }) => {
+    const ref = useRef(null)
+    const isInView = useInView(ref, { margin: '0px 0px -200px 0px' })
+
     return (
-        <div className="text-white">
+        <div
+            ref={ref}
+            style={{
+                transform: isInView ? 'translateY(0px)' : 'translateY(100px)',
+                opacity: isInView ? 1 : 0,
+                transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+            }}
+            className="text-white"
+        >
             <h2 className="text-base font-semibold">{heading}</h2>
             <h3 className="text-[28px] font-bold mt-3">{title}</h3>
             <p className="text-base mt-1 mb-6 text-gray-text">{subtitle}</p>
